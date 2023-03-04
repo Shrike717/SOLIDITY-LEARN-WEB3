f56c78c3a7094ff8d34981c0bb3a5decc8e1816b
Daniel Bauer <dan717@gmx.de>
2023-03-04 11:45:10 +0100

    Intial commit

---
+++ 1.7.0 Freshman - Intro to Solidity/contracts/HelloWorld.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.10;
+
+contract HelloWorld {
+    // Creating a public variable.
+    // Readable in the contract an from outside
+    string public greet = "Hello World!";
+}
